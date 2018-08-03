b7ba83f8d8678289198e7f4cee0ed16254f6dd2f
prampey <prudhvi.rampey@gmail.com>
2018-08-03 13:43:02 -0400

    Added testing pipeline to evaluate on LFW

--- utils/lfw.py
+++ utils/lfw.py
@@ -29,7 +29,7 @@ from __future__ import print_function
 
 import os
 import numpy as np
-from . import facenet
+import facenet
 
 def evaluate(embeddings, actual_issame, nrof_folds=10, distance_metric=0, subtract_mean=False):
     # Calculate evaluation metrics
